b459474b4d264b948f6246b9f95444e4e3ddc763
Shweta Gupta <shg@microsoft.com>
2017-03-28 09:11:09 +0530

    Modified file

--- processcsv.py
+++ processcsv.py
@@ -7,8 +7,8 @@ import argparse
 
 #### TODO: Get the configuration
 
-storage_acc_name = 'gecqrhbase'
-storage_acc_key = '2V/2YE0CwGYsb9YdqCKSBdNyZel0BFo8ommVZQiocfCcDw/iqotPAPx93N0wLM8TZZ2sqQZJjyT4+fDXK25Bng=='
+storage_acc_name = 'batchdevsgsa'
+storage_acc_key = 'wbGO9mskBhUShOq8G9RJHQeWiPtRRDVQn0CFrxuqPxD4PYoWOfwiIZntMzSTVZgJvnQjmJqqEu6EV5yRw2h3VQ=='
 folder_prefix = "cqr-cps-"
 block_blob_service = BlockBlobService(account_name=storage_acc_name,account_key=storage_acc_key)
 ####
@@ -20,33 +20,40 @@ def getfilename(name):
 
 
 
-def processcsvfile(fname,seperator,outdir,outfname,tblname):
-    block_blob_service.get_blob_to_path(container_name='metafiles',blob_name='headers.json',file_path='headers.json')
+def processcsvfile(fname,seperator,outdir,outfname):
     
-    with open('headers.json') as headervals:
-        jsonkeys = json.load(headervals)
-    header = jsonkeys[tblname.lower()]
-        
+    #block_blob_service.get_blob_to_path(container_name='metafiles',blob_name='headers.json',file_path='headers.json')
+    
+    #with open('headers.json') as headervals:
+    #    jsonkeys = json.load(headervals)
+    #header = jsonkeys[tblname.lower()]
+    #isHeaderLoaded = False
+    header = []    
     with open(fname) as inpfile:
         
         allrows = csv.reader(inpfile,delimiter=seperator)
         print("loaded file " + fname)
         all_vals = []
-        for rows in allrows:
-            i = 0
-            line = "{"
-            
-            for r in rows:
-                if i == 0:
-                    #line = line + '"' + header[i] + '":"' + r.replace(chr(10),'').replace(chr(13),'') + '"'
-                    line = line + '"' + header[i] + '":"' + r.decode('utf-8','ignore') + '"'
-                else:
-                    #line = line + "," + '"' + header[i] + '":"' + r.replace(chr(10),'').replace(chr(13),'') + '"'
-                    line = line + "," + '"' + header[i] + '":"' + r.decode('utf-8','ignore') + '"'
-                i = i + 1
 
-            line = line + "}"            
-            #print("line : " + line)
+        for rows in allrows:
+            if isHeaderLoaded is False:
+                header = rows
+            else:
+                if len(header) > 0:
+                    i = 0
+                    line = "{"
+                    
+                    for r in rows:
+                        if i == 0:
+                            
+                            line = line + '"' + header[i] + '":"' + r.decode('utf-8','ignore') + '"'
+                        else:
+                            
+                            line = line + "," + '"' + header[i] + '":"' + r.decode('utf-8','ignore') + '"'
+                        i = i + 1
+
+                    line = line + "}"            
+                    
 
             all_vals.append(json.loads(json.dumps(line)))
             line = ""
@@ -64,29 +71,58 @@ if __name__ == "__main__":
     ### TODO ### Replace day and month variable with calculating using datetime
 
     parser = argparse.ArgumentParser(description="Processes and stores data into hbase")
-    parser.add_argument("--year",dest="year")
-    parser.add_argument("--month",dest="month")
-    parser.add_argument("--day",dest="day")
-    parser.add_argument("--memberObj",dest="memberobj")
-    parser.add_argument("--hour",dest="hour")
+    parser.add_argument("--container",dest="container")
+    parser.add_argument("--pattern",dest="pattern")
+    #parser.add_argument("--month",dest="month")
+    #parser.add_argument("--day",dest="day")
+    #parser.add_argument("--memberObj",dest="memberobj")
+    #parser.add_argument("--hour",dest="hour")
     args = parser.parse_args()
 
     #memberObj = json.loads(args.memberobj)
-    
-    year = args.year
-    day = args.day
-    month = args.month
-    if args.hour is not None:
-        hour = args.hour
+    container = args.container
+    if args.pattern is not None:
+        pattern = args.pattern
     else:
-        hour = None
+        pattern = None
+
+    print("Processing files from container : " + str(container))
+    if pattern is not None:
+        generator = block_blob_service.list_blobs(container_name= container, prefix= pattern)
+    else:
+        generator = block_blob_service.list_blobs(container_name= container)
+    
+    for blob in generator:
+        print("Processing blob : " + blob.name)
+        blob_name = getfilename(blob.name)
+        downloadedblob = "downloaded_" + blob_name 
+        block_blob_service.get_blob_to_path(container_name=container,blob_name=blob.name, file_path=downloadedblob, open_mode='w')
+        if pattern is not None:
+            json_outpath = processcsvfile(fname=downloadedblob,seperator="|",outfname=blob_name,outdir='jsonfiles/'+ container + "/" + pattern)
+            print("uploading blob" + json_outpath)
+            block_blob_service.create_blob_from_path(container_name=container,blob_name=str(pattern+ 'json/' +blob_name+".json"),file_path=json_outpath) 
+        else:
+            json_outpath = processcsvfile(fname=downloadedblob,seperator="|",outfname=blob_name,outdir='jsonfiles/'+ container + "/",tblname=folder)
+            print("uploading blob" + json_outpath)
+            block_blob_service.create_blob_from_path(container_name=container,blob_name=str('json/' +blob_name+".json"),file_path=json_outpath) 
+        
+        
+        
+            
+    #year = args.year
+    #day = args.day
+    #month = args.month
+    #if args.hour is not None:
+    #    hour = args.hour
+    #else:
+    #    hour = None
 
     
-    print("member details:" + args.memberobj)
-    print("year : " + year)
-    print("month : " + month)
-    print("hour : " + hour)
-    print("day : " +day)
+    #print("member details:" + args.memberobj)
+    #print("year : " + year)
+    #print("month : " + month)
+    #print("hour : " + hour)
+    #print("day : " +day)
     #year = "2016"
     #day = "14"
     #month = "11"
